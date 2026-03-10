96f2465c3a6d4cd138a6242efc2c328a1ce89d7d
vtrlimdev7 <793vtrl.r@gmail.com>
2026-03-10 20:07:48 -0300

    Add navigation toggle functionality

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const navToggle = document.getElementById('nav-toggle');
+const navMenu = document.getElementById('nav-menu');
+
+navToggle.addEventListener('click', () => {
+  navMenu.classList.toggle('show');
+});
